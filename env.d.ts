3aec6adeeb6cf7f8a9760a925a5100d2522828f2
Malkalteer <alteermalk@gmail.com>
2026-01-19 12:24:11 +0200

    Esc Add Header component
    Esc
    Esc
    Esc
    Esc
    :wq
    :wq#

--- env.d.ts
+++ env.d.ts
@@ -7,3 +7,15 @@ interface ImportMeta {
 }
 
 export {};
+
+
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_API_KEY?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+export {};
+
